e500a1ed1b5b4002e601191db88c66213aec9a72
Michal <michal.bluj@cern.ch>
2015-09-11 16:14:13 +0200

    Cleaning

--- HTT/README.md
+++
@@ -1,2 +0,0 @@
-# usercode-WawTools
-git clone https://github.com/mbluj/usercode-WawTools.git WarsawAnalysis/HTT
--- HTTDataFormats/plugins/SealModule.cc
+++
@@ -1,9 +0,0 @@
-// Define plugin
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "WarsawAnalysis/HTT/plugins/MiniAODVertexAnalyzer.h"
-#include "WarsawAnalysis/HTT/plugins/MiniAODGenTauTauAnalyzer.h"
-
-DEFINE_FWK_MODULE(MiniAODVertexAnalyzer);
-DEFINE_FWK_MODULE(MiniAODGenTauTauAnalyzer);
-
