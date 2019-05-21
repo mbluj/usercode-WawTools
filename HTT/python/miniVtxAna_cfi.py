691001c7e095afadc6610166c02dd7547a487307
Michal Bluj <michal.bluj@cern.ch>
2019-05-21 11:55:38 +0200

    Update to 102X and various improvements

---
+++ HTT/python/miniVtxAna_cfi.py
@@ -0,0 +1,19 @@
+import FWCore.ParameterSet.Config as cms
+
+miniVtxAna = cms.EDAnalyzer(
+    "MiniAODVertexAnalyzer",
+    src = cms.InputTag("packedPFCandidates"),
+    lostSrc = cms.InputTag("lostTracks"),
+    lostEleSrc = cms.InputTag("lostTracks:eleTracks"),
+    genSrc = cms.InputTag("prunedGenParticles"),
+    #pruned = cms.InputTag("genParticles"),
+    vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    vertexScores = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    beamSpot = cms.InputTag("offlineBeamSpot"),
+    taus = cms.InputTag("slimmedTaus"),
+    muons = cms.InputTag("slimmedMuons"),
+    useBeamSpot = cms.bool(True),
+    useLostCands = cms.bool(True),
+    useTauTracks = cms.untracked.bool(True),
+    verbose = cms.untracked.bool(False),
+)
